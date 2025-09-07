ed4f994d9ff83f654a6a806526d3e0e86ce3dc18
trishitchar <trishitchar@gmail.com>
2025-09-07 16:00:07 +0530

    sitemap updated

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,9 +5,9 @@ export default function sitemap(): MetadataRoute.Sitemap {
 
 	return [
 		{ url: baseUrl, lastModified: new Date(), changeFrequency: 'weekly', priority: 1 },
-		{ url: `${baseUrl}/#skills`, lastModified: new Date(), changeFrequency: 'monthly', priority: 0.8 },
-		{ url: `${baseUrl}/#experience`, lastModified: new Date(), changeFrequency: 'monthly', priority: 0.8 },
-		{ url: `${baseUrl}/#projects`, lastModified: new Date(), changeFrequency: 'weekly', priority: 0.9 },
-		{ url: `${baseUrl}/#contact`, lastModified: new Date(), changeFrequency: 'monthly', priority: 0.7 },
+		{ url: `${baseUrl}/projects`, lastModified: new Date(), changeFrequency: 'monthly', priority: 0.8 },
+		{ url: `${baseUrl}/experience`, lastModified: new Date(), changeFrequency: 'monthly', priority: 0.8 },
+		{ url: `${baseUrl}/blog`, lastModified: new Date(), changeFrequency: 'weekly', priority: 0.9 },
+		{ url: `${baseUrl}/contact`, lastModified: new Date(), changeFrequency: 'monthly', priority: 0.7 },
 	]
 }
